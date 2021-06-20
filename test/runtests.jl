183d2f520e4183e2dde174467930243ad40e832e
Bill <wherrera10@users.noreply.github.com>
2021-06-20 13:38:14 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,15 @@
 using Gtk, DoseCalculators
 
 function amoxicillin_po(; age, weight, height)
-        if age <= 3/12
-            return weight * 30.0
-        elseif age <= 16 && weight <= 40
-            return weight * 37.0
-        else
-            return 1500.0
-      end
-     
+    if age <= 3/12
+        return weight * 30.0
+    elseif age <= 16 && weight <= 40
+        return weight * 37.0
+    else
+        return 1500.0
+    end
+end
+
 dose_calculator_app(amoxacillin_po, "Amoxacillin PO Dosing", "Amoxacillin dosage(mg)")
 
 win = DoseCalculators.apps[end]
@@ -16,4 +17,3 @@ win = DoseCalculators.apps[end]
 @test win isa GtkWindow
 
 Gtk.destroy(win)
-
